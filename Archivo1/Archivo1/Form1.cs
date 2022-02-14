4017c64f095d241c6d8186e3cd0f953c4818c6d0
Jantonio170 <jantonio_170b@hotmail.com>
2022-02-13 20:33:16 -0600

    Stack
    Se probo la funcion stack

--- Archivo1/Archivo1/Form1.cs
+++ Archivo1/Archivo1/Form1.cs
@@ -23,7 +23,20 @@ namespace Archivo1
         private void button1_Click(object sender, EventArgs e)
         {
             pila.Push(textBox1.Text);
+            textBox1.Clear();
+            letras();
+        }
 
+        public void letras()
+        {
+            string[] array2 = new string[pila.Count];
+            pila.CopyTo(array2, 0);
+            string let = "";
+            for (int i=0; i<pila.Count; i++)
+            {
+                let = let + array2[i];
+                label1.Text = let;
+            }
         }
     }
 }
