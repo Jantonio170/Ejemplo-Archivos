5f5ee9755cc0ead5b441fa3de3c4b4fcedea7e78
Jantonio170 <jantonio_170b@hotmail.com>
2022-02-13 20:06:49 -0600

    Prueba 2
    Se trata de subir un proyecto de c#

---
+++ Archivo1/Archivo1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivo1
+{
+    public partial class Form1 : Form
+    {
+        Stack pila = new Stack();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pila.Push(textBox1.Text);
+
+        }
+    }
+}
